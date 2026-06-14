6c9eaecaa07eac1dc1ca4f4e19a821bab50bdb7d
rahultumpala <rhltumpala@gmail.com>
2026-06-14 09:16:01 +0530

    Rebalance

--- ex-dsa/lib/dsa/bst.ex
+++ ex-dsa/lib/dsa/bst.ex
@@ -38,16 +38,57 @@ defmodule DSA.BST do
     tree
   end
 
+  @doc """
+  naive rebalance. takes 2x memory. converts tree to list and rebuilds tree.
+  """
+  def rebalance(tree) do
+    list = bst_to_list(tree)
+    count = Enum.count(list)
+    build_from_list(list, Kernel.div(count, 2), 0, count - 1)
+  end
+
+  def build_from_list(list, root_idx, list_start_idx, list_end_idx) do
+    cond do
+      # only 1 element
+      list_start_idx == list_end_idx ->
+        [Enum.at(list, list_end_idx), [], []]
+
+      # only 2 elements
+      list_end_idx - list_start_idx + 1 == 2 ->
+        [Enum.at(list, list_end_idx), [Enum.at(list, list_start_idx), [], []], []]
+
+      # out of bounds
+      root_idx < list_start_idx || root_idx > list_end_idx ->
+        []
+
+      true ->
+        [
+          Enum.at(list, root_idx),
+          build_from_list(
+            list,
+            div(root_idx - list_start_idx + 1, 2),
+            list_start_idx,
+            root_idx - 1
+          ),
+          build_from_list(
+            list,
+            root_idx + div(list_end_idx - root_idx + 1, 2),
+            root_idx + 1,
+            list_end_idx
+          )
+        ]
+    end
+  end
+
   def print(tree) do
-    print_all(tree)
+    bst_to_list(tree) |> Enum.each(&IO.write("#{&1} "))
     IO.puts("")
+    tree
   end
 
-  defp print_all([root, left, right] = tree) do
-    print_all(left)
-    IO.write("#{root} ")
-    print_all(right)
+  def bst_to_list([root, left, right]) do
+    bst_to_list(left) ++ [root] ++ bst_to_list(right)
   end
 
-  defp print_all([] = _tree), do: nil
+  def bst_to_list([]), do: []
 end
--- ex-dsa/test/bst_test.exs
+++ ex-dsa/test/bst_test.exs
@@ -3,11 +3,19 @@ defmodule BstTest do
 
   alias DSA.BST
 
-  test "insert and print test" do
-    numbers = [42, 7, 91, 18, 63, 5, 77, 30, 56, 14]
+  test "insert and rebalance test" do
+    numbers = [91, 7, 42, 18, 63, 5, 77, 30, 56, 14]
     bst = numbers |> Enum.reduce(BST.init(), fn el, tree -> DSA.BST.insert(tree, el) end)
-    bst |> IO.inspect()
-    bst |> BST.print()
+    sorted = Enum.sort(numbers)
 
+    # bst |> IO.inspect()
+    # bst |> BST.print()
+
+    rebalanced = bst |> BST.rebalance()
+
+    # rebalanced |> IO.inspect() |> BST.print()
+
+    assert sorted == bst |> BST.bst_to_list()
+    assert sorted == rebalanced |> BST.bst_to_list()
   end
 end
